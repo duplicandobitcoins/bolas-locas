be94fa9eebe371f72246583bc83e6d19429243c2
duplicandobitcoins <duplicandobitcoins@gmail.com>
2025-02-04 23:51:43 -0500

    Fix start command

--- bolas_locas/main.py
+++ bolas_locas/main.py
@@ -4,8 +4,20 @@ from fastapi.responses import JSONResponse
 import mysql.connector
 from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
 from bolas_locas.webhook import router as webhook_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://www.solutions-systems.com"],  # Asegúrate de que este es el dominio correcto
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los headers
+)
+
 
 app.include_router(webhook_router)
 
